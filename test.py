15e09431228106c4d452fccf7198558b6602f1f2
cloudmlops <84102103+cloudmlops@users.noreply.github.com>
2026-04-25 09:37:28 +0100

    Fix duplicate print statements in test.py

--- test.py
+++ test.py
@@ -4,4 +4,5 @@ print("this is code added in github UI")
 print("adding this code for fixing the change")
 print("adding this code for fixing the change")
 print("adding this code for fixing the change")
+print("this is code added in github UI")
 
