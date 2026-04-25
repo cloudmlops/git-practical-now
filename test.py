ec52f73149976c2be6bd1b39a6a223bdf129141e
cloudmlops <84102103+cloudmlops@users.noreply.github.com>
2026-04-25 09:48:49 +0100

    Correct indentation and add conditional logic
    Fix indentation and add conditional statement.

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import os
-print("this is os import code")
+  print("this is os import code")
+if a>10:
+else:
 print("this is code added in github UI")
 print("adding this code for fixing the change")
