e516d33ecc7a775204c58f820158fd20abf1c52d
cloudmlops <84102103+cloudmlops@users.noreply.github.com>
2026-04-25 09:50:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 import os
-  print("this is os import code")
+print("this is os import code")
+a=15
 if a>10:
+  print("a is greater")
 else:
+  print("a is smaller")
 print("this is code added in github UI")
 print("adding this code for fixing the change")
